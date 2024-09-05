b6137c18626905a5b2f6951a839f28503952659b
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-09-05 14:41:53 +0000

    added report

--- src/main/java/concurrency/Main.java
+++ src/main/java/concurrency/Main.java
@@ -1,73 +1,50 @@
 package concurrency;
 
-import concurrency.deadlock.DeadLockArrayList;
-import concurrency.forkjoin.MaxValueTask;
-import concurrency.threadInteruption.ArrayPrinterTask;
-
 import java.util.Arrays;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
+    private static AtomicInteger counter = new AtomicInteger();
 
     public static void main(String[] args) throws InterruptedException {
 
-        // testing fork join pool
 
- /*       int[] arr = new int[100];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (int) (Math.random() * 1000);
-        }
-
-        ForkJoinPool pool = new ForkJoinPool();
-        MaxValueTask task = new MaxValueTask(arr, 0, arr.length);
-        int result = pool.invoke(task);
-        System.out.println("Maximum Value: " + result);*/
-
-        // testing thread deadlock
-
-/*        DeadLockArrayList deadLockArrayList = new DeadLockArrayList();
-
-        Thread threadOne = new Thread(() -> {
-            try {
-                deadLockArrayList.threadOneAddToList();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                counter.incrementAndGet();
             }
 
-        });
+        };
 
-        Thread threadTwo = new Thread(() -> {
-            try {
-                deadLockArrayList.threadTwoAddToList();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
+        Thread thread = new Thread(task);
+        Thread thread2 = new Thread(task);
 
-        threadOne.start();
-        threadTwo.start();
+        thread.start();
+        thread2.start();
+        thread.join();
+        thread2.join();
 
-        threadOne.join();
-        threadTwo.join();
-
-        System.out.println("Task complete");*/
-
-        // testing thread interruption
-        int[] numbers = new int[300];
-        Arrays.setAll(numbers, i -> i * 10);
-
-        Thread taskThread = new Thread(new ArrayPrinterTask(numbers));
-        taskThread.start();
-
-        Thread.sleep(5000);
-
-        taskThread.interrupt();
-
-        taskThread.join();
-
-        System.out.println("Thread completed successfully");
+        System.out.println("Final counter value: " + counter.get());
 
 
+       /* PrintQueue printQueue = new PrintQueue(3);
+
+        Printer printer = new Printer(printQueue);
+
+        User user1 = new User(printQueue, "Document 1");
+        User user2 = new User(printQueue, "Document 2");
+        User user3 = new User(printQueue, "Document 3");
+        User user4 = new User(printQueue, "Document 4");
+        User user5 = new User(printQueue, "Document 5");
+
+        printer.start();
+        user1.start();
+        user2.start();
+        user3.start();
+        user4.start();
+        user5.start();
+*/
 
 
     }
